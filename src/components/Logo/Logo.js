f079f2f1ba7a6c9cc7683cde9600ac3e6d244fc8
DevTaehong <minth1123@icloud.com>
2022-10-13 14:38:48 -0300

    Changing logo image

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Tilt from 'react-parallax-tilt';
 import './Logo.css';
-import brain from './brain.png';
+import robot from './robot.png';
 
 const Logo = () => {
     return (
         <div className='ma4 mt0 safariZIndex'>
             <Tilt className='Tilt br2 shadow-2' style={{ height: '150px', width: '150px' }}>
                 <div className='pa3'>
-                    <img style={{paddingTop: '5px'}} alt='brain' src={brain}/>
+                    <img style={{paddingTop: '5px'}} alt='brain' src={robot}/>
                 </div>
             </Tilt>
         </div>
